711aa015dad9c8bfbe1f1453fedda0784d98b03f
nutnai <nutnai3302@gmail.com>
2024-03-24 12:59:49 +0700

    initial push test

---
+++ src/app/login/page.tsx
@@ -0,0 +1,7 @@
+export default function loginPage() {
+    return (
+        <div>
+            <h1>login page</h1>
+        </div>
+    );
+}
---
+++ src/app/test/page.tsx
@@ -0,0 +1,7 @@
+export default function testPage() {
+    return (
+        <div>
+            <h1>test page</h1>
+        </div>
+    );
+}
