4471dcfc563880317f926b58afb1d8be82aff103
Camppe <6434413323@student.chula.ac.th>
2024-03-31 16:14:50 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,20 +4,24 @@ export default function Home() {
   return (
     <>
       <Header />
-      <div className="mx-auto p-4 min-h-screen bg-white w-full px-20 ">
-        <div className="flex space-x-32 ">
-          <div className="flex-row border-solid rounded-lg bg-blue-500 p-5 mt-5 w-1/3 h-auto justify-center">
-            <div className="bg-white w-8/9 h-1/6 pt-6 my-6"></div>
-            <div className="bg-white w-8/9 h-1/6 pt-6 my-6"></div>
-            <div className="bg-white w-8/9 h-1/6 pt-6 my-6"></div>
+      <div className="mx-auto p-4 min-h-screen bg-white w-full pt-14 px-10 sm:px-20 lg:px-40">
+        <div className="flex flex-col sm:flex-row space-y-6 sm:space-y-0 sm:space-x-10">
+          <div className="flex flex-col justify-between border-solid rounded-lg bg-blue-500 p-5 mb-5 sm:mb-0 sm:pb-14 w-full sm:w-2/6 lg:w-1/3 xl:w-1/4">
+            <div className="space-y-6">
+              <div className="bg-white w-8/9 h-4 sm:h-6 md:h-8 lg:h-10 xl:h-12"></div>
+              <div className="bg-white w-8/9 h-4 sm:h-6 md:h-8 lg:h-10 xl:h-12"></div>
+              <div className="bg-white w-8/9 h-4 sm:h-6 md:h-8 lg:h-10 xl:h-12"></div>
+            </div>
             <button
               type="submit"
-              className=" flex justify-center w-8/9 h-1/6 pt-6 my-6 w-full border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-300 hover:bg-indigo-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              className="w-full h-12 mt-6 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-300 hover:bg-indigo-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
               Search
             </button>
           </div>
-          <div className="container border-solid rounded-lg border-8 border-yellow-200 p-5 mt-5 w-2/3"></div>
+          <div className="container border-solid rounded-lg bg-yellow-200 p-5 w-full">
+            {/* Your content here */}
+          </div>
         </div>
       </div>
     </>
