2c130a759c5069623ff1fc078fc86245310dac6d
DewGoody <dewzky1@gmail.com>
2024-04-24 18:15:14 +0700

    change color and create payment

--- app/components/hoteldetail.tsx
+++ app/components/hoteldetail.tsx
@@ -14,14 +14,14 @@ const HotelDetail: React.FC<HotelDetailProps> = ({ location, facilities, guests,
     <div className="ml-16 bg-white p-6 flex justify-evenly">
       <div className='mt-3'>
         <div className="mb-4">
-          <h3 className="text-lg text-indigo-600 font-semibold mb-2">Location:</h3>
-          <p className="text-indigo-500">{location}</p>
+          <h3 className="text-lg text-gray-900 font-semibold mb-2">Location:</h3>
+          <p className="text-gray-800">{location}</p>
         </div>
         <div className="mb-4">
-          <h3 className="text-lg mt-9 text-indigo-600 font-semibold mb-2">Facilities:</h3>
+          <h3 className="text-lg mt-9 text-gray-900 font-semibold mb-2">Facilities:</h3>
           <ul className="list-disc list-inside">
             {facilities.map((facility, index) => (
-              <li key={index} className="text-indigo-500">
+              <li key={index} className="text-gray-800">
                 {facility}
               </li>
             ))}
@@ -29,23 +29,23 @@ const HotelDetail: React.FC<HotelDetailProps> = ({ location, facilities, guests,
         </div>
       </div>
       <div className="mt-3">
-        <h3 className="text-lg text-indigo-600 font-semibold mb-0">Guest and Price:</h3>
+        <h3 className="text-lg text-gray-900 font-semibold mb-0">Guest and Price:</h3>
         <table className="table-auto mt-3">
           <thead>
             <tr>
-              <th className=" text-indigo-500 px-4 py-2">Guest</th>
-              <th className="text-indigo-500 px-4 py-2">Price</th>
+              <th className=" text-gray-800 px-4 py-2">Guest</th>
+              <th className="text-gray-800 py-2">Price</th>
             </tr>
           </thead>
           <tbody>
             {guests.map((guestOption, index) => (
               <tr key={index}>
-                <td className=" text-indigo-500 border px-4 py-2">
+                <td className=" text-gray-800 border px-4 py-2">
                   <ul>
                     <li>{guestOption} guests</li>
                   </ul>
                 </td>
-                <td className="text-indigo-500 border px-4 py-2">
+                <td className="text-gray-800 border px-4 py-2">
                   <ul>
                     <li>{prices[index]} Baht per night</li>
                   </ul>
